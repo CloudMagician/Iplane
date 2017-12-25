8bc280d162b6a7b237f93af5b15a2b59fcb2c7ac
jetfire948 <382787300@qq.com>
2017-12-25 09:48:33 +0800

    Update again!

--- Airplane_System.hpp
+++ Airplane_System.hpp
@@ -17,21 +17,22 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include ""
 using namespace std;
 
 //定义一个航班信息的类
 class flight
 {
 public:
-	string flight_number;        //航班号
-	string starting_point;   //起始城市
-	string finishing_point;   //到达城市
-	int strat_time;              //起飞时间
-	int finish_time;            //到达时间
-	int price;                      //飞机票价
-	int discount;               //票价折扣
-	string company;           //所属公司
-	int seat_number;          //座位总数
+	string flight_number;   //航班号
+	string starting_point;  //起始城市
+	string finishing_point; //到达城市
+	int start_time;         //起飞时间
+	int finish_time;        //到达时间
+	int price;              //飞机票价
+	int discount;           //票价折扣
+	string company;         //所属公司
+	int seat_number;        //座位总数
 	int book_number;        //已预定总数
 };
 
@@ -39,40 +40,41 @@ public:
 class Airplane_System
 {
 public:
-    Airplane_System();              //构造函数
-    ~Airplane_System();             //析构函数
-	void dataloading();										  //航班信息录入
-	void current_information_view();					  //航班信息浏览
-	void check_information();							      //航班信息查询
-	void ticket_booking();									  //预订机票
-	void ticket_returning(string flight_num);	//退票(航班号)
-	void flight_list();											  //相应航班排序
-	void flight_recommended();						      //航班线路设计
-	void output_route();									      //航班网络输出
-	void best_route_recommended();			      //最优航班线路推荐
+    Airplane_System();                                  //构造函数
+    ~Airplane_System();                                 //析构函数
+	void dataloading();							        //航班信息录入
+	void current_information_view();			        //航班信息浏览
+	void check_information();					        //航班信息查询
+	void ticket_booking();						        //预订机票
+	void ticket_returning(string);                      //退票(航班号)
+	void flight_list(int,string,string,int,int);        //相应航班排序
+	void flight_recommended();					        //航班线路设计
+	void output_route();						        //航班网络输出
+	void best_route_recommended();			            //最优航班线路推荐
 private:
     //边链表
 	struct edge
 	{
-		int takeoff_point;                                      //起飞城市
-		edge* nextpoint;                                          //到达城市
-		flight* flight_node;                                     //航班信息节点
+		int takeoff_point;      //起飞城市
+		edge* nextpoint;        //到达城市
+		flight* flight_node;    //航班信息节点
 	};
     //定点数组
 	struct point
 	{
-		int takeoffpoint;                                         //起飞城市
-		string cityname;                                         //城市名称
-		edge* address;                                          //边链表的头指针
+		int takeoffpoint;   //起飞城市
+		string cityname;    //城市名称
+		edge* address;      //边链表的头指针
 	};
-    int number_of_city;                                            //开放航线的城市个数
-    int total;                                                            //航班总数
-    string number1[300];                                        //过渡string数组，无重要作用，可忽略
-	flight* flight_totalnumber;                         //动态flight类数组的数组名（指针）
-	bool time_compare(int, int);                         //时间比较（是否可以并入航线）,第一个参数是航班的落地时间，第二个参数是航班的起飞时间
-	bool place_compare(string, string);                //地点比较，比较两个地点是否为同一地点
+    int number_of_city;                 //开放航线的城市个数
+    int total;                          //航班总数
+    string number1[300];                //过渡string数组，无重要作用，可忽略
+	flight* flight_totalnumber;         //动态flight类数组的数组名（指针）
+	bool time_compare(int, int);        //时间比较（是否可以并入航线）,第一个参数是航班的落地时间，第二个参数是航班的起飞时间
+	bool place_compare(string, string); //地点比较，比较两个地点是否为同一地点
     
-    void print_lzx(int);                    //陆子旭定义的相关输出函数
+    void print_flight_data(flight);     //输出航班信息
+    void print_lzx(int);                //陆子旭定义的相关输出函数
 };
 
 //构造函数
@@ -83,6 +85,13 @@ Airplane_System::Airplane_System(){
 //析构函数
 Airplane_System::~Airplane_System(){
     
+}
+
+//输出航班信息
+void print_flight_data(flight temp){
+    cout << temp.string flight_number << '\t' << temp.starting_point << '\t' << temp.finishing_point << '\t'
+    << temp.start_time << '\t' << temp.finish_time << '\t' << temp.price << '\t' << temp.discount << '\t'
+    << temp.company << '\t' << temp.seat_number << '\t' << temp.book_number << endl;
 }
 
 //时间比较（是否可以并入航线）,第一个参数是航班的落地时间，第二个参数是航班的起飞时间
@@ -115,8 +124,11 @@ void Airplane_System::dataloading()
 	}
 	string number;
 	getline(file, number);
-	total = stoi(number);                                                       //取出一共有几个航班			
+	total = stoi(number);   //取出一共有几个航班
 	flight_totalnumber = new flight[total];
+    while (!file.eof()) {
+        <#statements#>
+    }
 	for (int i = 0; i <total; i++)
 	{
 		getline(file, number1[i]);
@@ -181,7 +193,38 @@ void Airplane_System::ticket_returning(string flight_num){
 }
 
 //相应航班排序 by 陆子旭
-void Airplane_System::flight_list(){
+//参数分别为：
+//升降序（sort_bool）： 1为时间从低到高，2为时间从高到低，3为价格从低到高，4为价格从高到低；
+//对于起飞时间起始时间限制（timef_bool）；
+//对于起飞时间结束时间限制（timel_bool）；
+//起始城市（start_bool）；
+//到达城市（arrive_bool）；
+void Airplane_System::flight_list(int sort_bool=1,int timef_bool=201708010000,int timel_bool=201708020000,
+                                  string start_bool="长春",string arrive_bool="长春"){
+    vector<flight> temp;    //临时中间量
+    int sum(0);             //总数
+    Qsort qsort;            //快速排序
+    //筛选
+    for (int i = 0; i <total; i++) {
+        if (flight_totalnumber[i].start_time > timef_bool&&
+            flight_totalnumber[i].start_time < timel_bool&&
+            flight_totalnumber[i].starting_point == start_bool&&
+            flight_totalnumber[i].starting_point == arrive_bool
+            ) {
+            temp.push_back(flight_totalnumber[i]);
+            sum++;
+        }
+    }
+    switch (sort_bool) {
+        case 1:
+            qsort.find(flight_totalnumber->price,sum,flight_totalnumber);
+            qsort.sort();
+            for (int i = 0; i <sum; i++) {
+                print(flight_totalnumber[i]);
+            }
+        default:
+            break;
+    }
     
 }
 #endif /* Airplane_System_hpp */
